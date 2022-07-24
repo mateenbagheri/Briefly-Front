2220b788434cd27230ec435db6a13d993204ae34
reza shakeri <r.shakeri9210@gmail.com>
2022-07-25 00:34:03 +0430

    [feat] [wip] intercept auth guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { ClassProvider, NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app.routing';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app.routing";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { AuthGuard } from "./core/guard/auth.guard";
+import { CoreModule } from "./core/core.module";
+
+const HTTP_AUTH_GUARD: ClassProvider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: AuthGuard,
+  multi: true,
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CoreModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [HTTP_AUTH_GUARD, AuthGuard],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./core/guard/auth.guard";
 
 const routes: Routes = [
+  {
+    path:'',
+    // canActivate:[AuthGuard]
+  },
   {
     path: "auth",
     loadChildren: () => import("./auth/auth.module").then((m) => m.AuthModule),
