f5f8a4bdde5a077aa3762f157e31d58fd3a55a22
reza shakeri <r.shakeri9210@gmail.com>
2022-07-25 00:16:40 +0430

    [feat] add auth route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "auth",
+    loadChildren: () => import("./auth/auth.module").then((m) => m.AuthModule),
+    // canActivate: [AuthGuard],
+  },
+  // { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
